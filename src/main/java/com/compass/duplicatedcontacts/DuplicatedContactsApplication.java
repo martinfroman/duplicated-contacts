c8d364655d5c1eb711b01615878437540fd47477
martinfroman <martin.fernando.roman@gmail.com>
2024-11-05 12:05:19 -0300

    first commit

---
+++ src/main/java/com/compass/duplicatedcontacts/DuplicatedContactsApplication.java
@@ -0,0 +1,13 @@
+package com.compass.duplicatedcontacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuplicatedContactsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuplicatedContactsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/compass/duplicatedcontacts/DuplicatedContactsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compass.duplicatedcontacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuplicatedContactsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
